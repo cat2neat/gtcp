7bf1c9cfc97b3dffbafbe4d36bb54943c408b715
cat2neat <cat2neat@gmail.com>
2017-01-08 22:14:10 +0900

    Add comments for limiter.go

--- limiter.go
+++ limiter.go
@@ -3,17 +3,25 @@ package gtcp
 import "sync/atomic"
 
 type (
+	// Limiter is the interface that limits connections accepted.
 	Limiter interface {
+		// OnConnected is called when Conn accepted on a listener.
+		// Returns false if limits it otherwise true.
 		OnConnected(Conn) bool
+		// OnClosed is called when Conn closed.
 		OnClosed(Conn)
 	}
 
+	// MaxConnLimiter implements Limiter based on the maximum connections.
 	MaxConnLimiter struct {
+		// Max defines the maximum connections.
 		Max     uint32
 		current uint32 // accessed atomically
 	}
 )
 
+// OnConnected returns false if the number of current active connections exceeds Max,
+// otherwise true.
 func (mc *MaxConnLimiter) OnConnected(conn Conn) bool {
 	new := atomic.AddUint32(&mc.current, 1)
 	if new > mc.Max {
@@ -23,6 +31,7 @@ func (mc *MaxConnLimiter) OnConnected(conn Conn) bool {
 	return true
 }
 
+// OnClosed decreases the number of current active connections.
 func (mc *MaxConnLimiter) OnClosed(conn Conn) {
 	atomic.AddUint32(&mc.current, ^uint32(0))
 }
