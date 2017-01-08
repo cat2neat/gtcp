3722d498f4e27300520c636a2b5fd00b7b938a42
cat2neat <cat2neat@gmail.com>
2017-01-08 21:59:15 +0900

    Add comments for log.go

--- log.go
+++ log.go
@@ -5,17 +5,22 @@ import (
 )
 
 type (
+	// Logger is the interface that wraps logging operations.
 	Logger interface {
+		// Errorf logs error information.
+		// Arguments are handled in the manner of fmt.Printf.
 		Errorf(format string, args ...interface{})
 	}
-
+	// BuiltinLogger implements Logger based on the standard log package.
 	BuiltinLogger struct{}
 )
 
 var (
+	// DefaultLogger is the default Logger.
 	DefaultLogger Logger = BuiltinLogger{}
 )
 
+// Errorf logs error information using the standard log package.
 func (l BuiltinLogger) Errorf(format string, args ...interface{}) {
 	log.Printf(format, args...)
 }
