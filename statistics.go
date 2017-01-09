a267a833133980aba05d47b012b715ed3ea6a9fc
cat2neat <cat2neat@gmail.com>
2017-01-09 11:46:06 +0900

    Add comments for statistics.go

--- statistics.go
+++ statistics.go
@@ -6,12 +6,17 @@ import (
 )
 
 type (
+	// Statistics is the interface that wraps operations for accumulating Conn statistics.
 	Statistics interface {
+		// AddConnStats adds a Conn statistics.
 		AddConnStats(Conn)
+		// Reset clears statistics holden now.
 		Reset()
+		// String returns a string that represents the current statistics.
 		String() string
 	}
 
+	// TrafficStatistics implements Statistics to hold the in/out traffic on a gtcp server.
 	TrafficStatistics struct {
 		mu       sync.RWMutex
 		inBytes  int64
@@ -19,6 +24,8 @@ type (
 	}
 )
 
+// AddConnStats ingests inBytes and outBytes from conn.
+// You need to use StatsConn in gtcp.NewConn if you want in/out traffic.
 func (ts *TrafficStatistics) AddConnStats(conn Conn) {
 	in, out := conn.Stats()
 	ts.mu.Lock()
@@ -27,12 +34,14 @@ func (ts *TrafficStatistics) AddConnStats(conn Conn) {
 	ts.mu.Unlock()
 }
 
+// Reset clears statistics holden now.
 func (ts *TrafficStatistics) Reset() {
 	ts.mu.Lock()
 	ts.inBytes, ts.outBytes = 0, 0
 	ts.mu.Unlock()
 }
 
+// String returns the in/out traffic on a gtcp server as a json string.
 func (ts *TrafficStatistics) String() (str string) {
 	ts.mu.RLock()
 	str = fmt.Sprintf(`{"in_bytes": %d, "out_bytes": %d}`, ts.inBytes, ts.outBytes)
